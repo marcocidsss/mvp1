b917361c8d45bf02091fbcfbe52f0be617bfb181
marco <marcoacid6@gmail.com>
2025-10-21 12:52:28 +0200

    fix added to __innit__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,12 +8,20 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
+
+    # Configuración de base de datos y JWT
+    DB_USER = os.environ.get("DB_USER", "root")
+    DB_PASS = os.environ.get("DB_PASS", "")
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
+    DB_NAME = os.environ.get("DB_NAME", "venue")
+
     app.config.from_mapping(
-        SECRET_KEY = os.environ.get('SECRET_KEY','change-me'),
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','mysql+pymysql://user:pass@db:3306/venue'),
-        SQLALCHEMY_TRACK_MODIFICATIONS = False,
-        JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY','change-me-too')
+        SECRET_KEY=os.environ.get('SECRET_KEY', 'change-me'),
+        SQLALCHEMY_DATABASE_URI=f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY', 'change-me-too')
     )
+
     db.init_app(app)
     jwt.init_app(app)
 
